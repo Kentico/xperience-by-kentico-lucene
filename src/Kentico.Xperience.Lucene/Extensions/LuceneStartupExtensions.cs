eb2418fdc56cc93abe0c6a41095eab70a5ce45c7
Sean G. Wright <sean@seangwright.me>
2024-01-09 14:51:29 -0500

    refactor(Lucene): DI/registration pattern and docs

--- src/Kentico.Xperience.Lucene/Extensions/LuceneStartupExtensions.cs
+++ src/Kentico.Xperience.Lucene/Extensions/LuceneStartupExtensions.cs
@@ -1,14 +1,17 @@
 ﻿using Kentico.Xperience.Lucene;
 using Kentico.Xperience.Lucene.Services;
+using Kentico.Xperience.Lucene.Services.Implementations;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
-/// <summary>
-/// Application startup extension methods.
-/// </summary>
 public static class LuceneStartupExtensions
 {
+    /// <summary>
+    /// Adds Lucene services and custom module to application using the <see cref="DefaultLuceneIndexingStrategy"/> for all indexes
+    /// </summary>
+    /// <param name="serviceCollection"></param>
+    /// <returns></returns>
     public static IServiceCollection AddLucene(this IServiceCollection serviceCollection)
     {
         serviceCollection.TryAddSingleton<LuceneModuleInstaller>();
@@ -16,9 +19,50 @@ public static class LuceneStartupExtensions
         return serviceCollection;
     }
 
-    public static IServiceCollection RegisterStrategy<TStrategy>(this IServiceCollection serviceCollection, string strategyName) where TStrategy : ILuceneIndexingStrategy, new()
+    /// <summary>
+    /// Adds Lucene services and custom module to application with customized options provided by the <see cref="ILuceneBuilder"/>
+    /// in the <paramref name="configure" /> action.
+    /// </summary>
+    /// <param name="serviceCollection"></param>
+    /// <param name="configure"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddLucene(this IServiceCollection serviceCollection, Action<ILuceneBuilder> configure)
+    {
+        serviceCollection.AddLucene();
+
+        var builder = new LuceneBuilder(serviceCollection);
+
+        configure(builder);
+
+        return serviceCollection;
+    }
+}
+
+public interface ILuceneBuilder
+{
+    /// <summary>
+    /// Registers the given <typeparamref name="TStrategy" /> as a transient service under <paramref name="strategyName" />
+    /// </summary>
+    /// <typeparam name="TStrategy">The custom type of <see cref="ILuceneIndexingStrategy"/> </typeparam>
+    /// <param name="strategyName">Used internally <typeparamref name="TStrategy" /> to enable dynamic assignment of strategies to search indexes. Names must be unique.</param>
+    /// <exception cref="ArgumentException">
+    ///     Thrown if an strategy has already been registered with the given <paramref name="strategyName"/>
+    /// </exception>
+    /// <returns></returns>
+    ILuceneBuilder RegisterStrategy<TStrategy>(string strategyName) where TStrategy : class, ILuceneIndexingStrategy;
+}
+
+internal class LuceneBuilder : ILuceneBuilder
+{
+    private readonly IServiceCollection serviceCollection;
+
+    public LuceneBuilder(IServiceCollection serviceCollection) => this.serviceCollection = serviceCollection;
+
+    public ILuceneBuilder RegisterStrategy<TStrategy>(string strategyName) where TStrategy : class, ILuceneIndexingStrategy
     {
         StrategyStorage.AddStrategy<TStrategy>(strategyName);
-        return serviceCollection;
+        serviceCollection.AddTransient<TStrategy>();
+
+        return this;
     }
 }
