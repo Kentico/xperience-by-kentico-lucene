f496336d03c059032534d72b4e8d6f0a59334b8a
bkapustik <82807109+bkapustik@users.noreply.github.com>
2024-07-30 14:56:10 +0200

    feat(Lucene): Web Farm Indexing Tasks

---
+++ src/Kentico.Xperience.Lucene.Core/Scaling/IndexLogWebPageItemWebFarmTask.cs
@@ -0,0 +1,29 @@
+﻿using CMS.Base;
+using CMS.Core;
+
+using Kentico.Xperience.Lucene.Core.Indexing;
+
+namespace Kentico.Xperience.Lucene.Core.Scaling;
+internal class IndexLogWebPageItemWebFarmTask : WebFarmTaskBase
+{
+    private readonly IEventLogService eventLog;
+    private readonly ILuceneTaskLogger luceneTaskLogger;
+    public string? CreatorName { get; set; }
+    public IndexEventWebPageItemModel? Data { get; set; }
+    public LuceneTaskType TaskType { get; set; }
+    public string? IndexName { get; set; }
+
+    public IndexLogWebPageItemWebFarmTask()
+    {
+        eventLog = Service.Resolve<IEventLogService>();
+        luceneTaskLogger = Service.Resolve<ILuceneTaskLogger>();
+    }
+
+    public override void ExecuteTask()
+    {
+        string message = $"Server {SystemContext.ServerName} is processing a Lucene indexing task from creator {CreatorName}";
+        eventLog.LogInformation("Lucene Indexing Task", "Execute", message);
+
+        luceneTaskLogger.LogIndexTask(new LuceneQueueItem(Data!, TaskType, IndexName!));
+    }
+}
