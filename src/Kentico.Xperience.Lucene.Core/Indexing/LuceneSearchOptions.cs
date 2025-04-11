0d4474bc2cd4e8e83a2f9f66598d50a9d8a020c2
bkapustik <boriskokap@gmail.com>
2025-04-11 11:10:16 +0200

    feat(Indexing Secured Items): options

---
+++ src/Kentico.Xperience.Lucene.Core/Indexing/LuceneSearchOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Kentico.Xperience.Lucene.Core.Indexing;
+
+/// <summary>
+/// Lucene search integration options.
+/// </summary>
+public sealed class LuceneSearchOptions
+{
+    /// <summary>
+    /// Configuration section name.
+    /// </summary>
+    public const string CMS_LUCENE_SEARCH_SECTION_NAME = "CMSLuceneSearch";
+
+    /// <summary>
+    /// If true, the items that require authentication will be included in the indexing process.
+    /// </summary>
+    public bool IncludeSecuredItems { get; set; } = false;
+}
