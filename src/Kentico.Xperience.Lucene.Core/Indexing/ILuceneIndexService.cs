564a62c7560734b28d56440e1e9f80c965a829e1
bkapustik <boriskokap@gmail.com>
2025-04-25 13:53:45 +0200

    fix(format): redundand access modifiers

--- src/Kentico.Xperience.Lucene.Core/Indexing/ILuceneIndexService.cs
+++ src/Kentico.Xperience.Lucene.Core/Indexing/ILuceneIndexService.cs
@@ -5,9 +5,9 @@ namespace Kentico.Xperience.Lucene.Core.Indexing;
 
 public interface ILuceneIndexService
 {
-    public T UseIndexAndTaxonomyWriter<T>(LuceneIndex index, Func<IndexWriter, ITaxonomyWriter, T> useIndexWriter, IndexStorageModel storage, OpenMode openMode = OpenMode.CREATE_OR_APPEND);
+    T UseIndexAndTaxonomyWriter<T>(LuceneIndex index, Func<IndexWriter, ITaxonomyWriter, T> useIndexWriter, IndexStorageModel storage, OpenMode openMode = OpenMode.CREATE_OR_APPEND);
 
-    public T UseWriter<T>(LuceneIndex index, Func<IndexWriter, T> useIndexWriter, IndexStorageModel storage, OpenMode openMode = OpenMode.CREATE_OR_APPEND);
+    T UseWriter<T>(LuceneIndex index, Func<IndexWriter, T> useIndexWriter, IndexStorageModel storage, OpenMode openMode = OpenMode.CREATE_OR_APPEND);
 
-    public void ResetIndex(LuceneIndex index);
+    void ResetIndex(LuceneIndex index);
 }
