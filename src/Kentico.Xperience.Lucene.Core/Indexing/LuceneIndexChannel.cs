9661d2f6ab704e6e3f63ac8e0a6da2b83b498b55
Boris Kapustík <boris.kapustik@promotron.com>
2025-08-29 13:11:07 +0200

    feat(multi channel support): code cleanup, add comments

--- src/Kentico.Xperience.Lucene.Core/Indexing/LuceneIndexChannel.cs
+++ src/Kentico.Xperience.Lucene.Core/Indexing/LuceneIndexChannel.cs
@@ -1,12 +1,28 @@
 ﻿namespace Kentico.Xperience.Lucene.Core.Indexing;
 
+/// <summary>
+/// The configuration for a specific website channel associated with a Lucene index.
+/// </summary>
 public class LuceneIndexChannel
 {
+    /// <summary>
+    /// The code name of the website channel.
+    /// </summary>
     public string ChannelName { get; set; } = string.Empty;
+
+
+    /// <summary>
+    /// The display name of the website channel.
+    /// </summary>
     public string ChannelDisplayName { get; set; } = string.Empty;
 
+
+    /// <summary>
+    /// The default constructor.
+    /// </summary>
     public LuceneIndexChannel()
     { }
+
 
     public LuceneIndexChannel(string channelName, string channelDisplayName)
     {
