873d8096ef1114d837c267c87576f677921df77d
bkapustik <boriskokap@gmail.com>
2025-09-11 18:48:51 +0200

    feat(multi channel support): disable eslint rule where unwanted

--- src/Kentico.Xperience.Lucene.Admin/Client/src/models/LuceneIncludedPathConfigurationProps.tsx
+++ src/Kentico.Xperience.Lucene.Admin/Client/src/models/LuceneIncludedPathConfigurationProps.tsx
@@ -5,5 +5,7 @@ export interface LuceneIncludedPathConfigurationProps
 {
     value: IncludedPath[];
     possibleContentTypeItems: LuceneIndexContentType[] | null;
+    /* eslint-disable @typescript-eslint/naming-convention */
     onChange?: (value: IncludedPath[]) => void;
+    /* eslint-enable @typescript-eslint/naming-convention */
 }
