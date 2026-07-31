5a2225dd5535cafa164925f963437c53d4696f0d
Josef Pohrom <josefp@kentico.com>
2026-07-31 11:08:25 +0200

    allow reindex after failed load

--- src/Kentico.Xperience.Lucene.Admin/UIPages/IndexListingPage.cs
+++ src/Kentico.Xperience.Lucene.Admin/UIPages/IndexListingPage.cs
@@ -91,7 +91,16 @@ public class IndexListingPage : ListingPage
     {
         var result = await base.LoadData(settings, cancellationToken);
 
-        var statistics = await luceneClient.GetStatistics(default);
+        ICollection<LuceneIndexStatisticsModel> statistics = [];
+        try
+        {
+            statistics = await luceneClient.GetStatistics(default);
+        }
+        catch (Exception ex)
+        {
+            EventLogService.LogException(nameof(IndexListingPage), nameof(LoadData), ex);
+        }
+
         // Add statistics for indexes that are registered but not created in Lucene
         AddMissingStatistics(ref statistics, indexManager);
 
